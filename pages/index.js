ec62807fd9bcc346cc4d057ad522ff7e10d79ee8
Vladimir Starkov <iamstarkov@gmail.com>
2019-04-02 03:37:45 +0200

    add favicons

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,15 @@ class PageIndex extends React.Component {
         <Head>
           <title>Topics Manager</title>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <link rel="apple-touch-icon" sizes="120x120" href="/static/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
+          <link rel="manifest" href="/static/site.webmanifest" />
+          <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#0000" />
+          <link rel="shortcut icon" href="/static/favicon.ico" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="msapplication-config" content="/static/browserconfig.xml" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <h1>Welcome to "Topics Manager"!</h1>
         <p>
