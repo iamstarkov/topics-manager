b0fcee4819100cc146ebf6068452cfcad569dd69
Vladimir Starkov <iamstarkov@gmail.com>
2019-04-02 03:54:21 +0200

    remove vertical padding from Wrapper

--- components/wrapper.js
+++ components/wrapper.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
   max-width: 30em;
   margin: 0 auto;
-  padding: 1em;
+  padding: 0 1em;
   color: #222;
   font-size: 1.4em;
 `;
