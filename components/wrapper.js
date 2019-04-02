dc3e2b77bd890338dcc4a431e7eb55dee0503d2a
Vladimir Starkov <iamstarkov@gmail.com>
2019-04-02 02:58:37 +0200

    finish with landing page

---
+++ components/wrapper.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  max-width: 30em;
+  margin: 0 auto;
+  padding: 1em;
+  color: #222;
+  font-size: 1.4em;
+`;
+
+export default Wrapper;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Link from 'next/link'
 import cookies from 'next-cookies';
 import fetch from 'isomorphic-unfetch';
+import Wrapper from '../components/wrapper';
 
 class PageIndex extends React.Component {
   static async getInitialProps(ctx) {
@@ -20,18 +21,29 @@ class PageIndex extends React.Component {
   }
   render() {
     const { token, user } = this.props;
-    if (!token) {
-      return <>
-        <Link href="/login">
-          <a>login</a>
-        </Link>
-      </>
-    }
     return <>
-      <h1>Hello, {user.login}</h1>
-      <Link href="/logout">
-        <a>logout</a>
-      </Link>
+      <Wrapper>
+        <h1>Welcome to "Topics Manager"!</h1>
+        <p>
+          Regardless of how many repositories you have on your GitHub account, we all can agree that its UI is not very convenient to use.
+        </p>
+        <p>
+          This website aims to address this issue. Topics Manager will give you an consistent dashboard to overview and edit topics on your GitHub repositories in bulk and one by one.
+        </p>
+        <p>
+         Topics Manager will need access to your GitHub account's repositories in order to manage its keywords.
+        </p>
+        {!token && (
+          <p>
+            <Link href="/login"><a>Login with GitHub</a></Link> to start.
+          </p> 
+        )}
+        {!!token && (
+          <p>
+            <Link href="/dashboard"><a>Open dashboard</a></Link> to start or <Link href="/logout"><a>logout</a></Link> to finish the session.
+          </p>
+        )}
+      </Wrapper>
     </>
   }
 }
