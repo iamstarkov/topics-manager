0b963fed383c024559acb0c2f7de8a6825d347c2
Vladimir Starkov <iamstarkov@gmail.com>
2019-04-02 05:33:13 +0200

    list repos on a dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -8,20 +8,36 @@ import Layout from '../components/layout';
 
 class PageDashboard extends React.Component {
   static async getInitialProps(ctx) {
+    const { res } = ctx;
     const { token } = cookies(ctx);
-    let user;
-    if (!!token) {
-      user = await api.get('/user', token);
+    if (!token) {
+      const redirectUrl = '/';
+      if (res) {
+        res.writeHead(302, { Location: redirectUrl });
+        res.end();
+        return;
+      } else {
+        Router.replace(redirectUrl)
+        return;
+      }
     }
-    return { token, user };
+    const [user, repos] = await Promise.all([
+      api.get('/user', token),
+      api.get('/user/repos?type=owner', token),
+    ]);
+
+    return { user, repos };
   }
   render() {
-    const { user } = this.props;
+    const { user, repos } = this.props;
     return <>
       <Layout title="Dashboard">
       <Wrapper>
         <h1>Dashboard</h1>
-        <h2>Hello, @{user.login}</h2>
+        <h2>Hello, @{user.login} <small>(<Link href="/logout"><a>logout</a></Link>)</small></h2>
+        <ul>
+          {repos.map(x => (<li key={x.id}>{x.full_name}</li>))}
+        </ul>
       </Wrapper>
       </Layout>
     </>
