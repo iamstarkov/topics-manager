ebf8fd22afd5a04d4cb418ee7b9b8f707f471a9a
Vladimir Starkov <iamstarkov@gmail.com>
2019-04-04 00:19:52 +0200

    polish index

--- pages/index.js
+++ pages/index.js
@@ -14,44 +14,41 @@ class PageIndex extends React.Component {
   render() {
     const { token } = this.props;
     return (
-      <>
-        <Layout>
-          <Wrapper>
-            <h1>Welcome to Topics Manager!</h1>
+      <Layout>
+        <Wrapper>
+          <h1>Welcome to Topics Manager!</h1>
+          <p>
+            This app aims to address inconvenience of default topics editing
+            interface. Topics Manager will give a dashboard to overview and edit
+            topics of your GitHub repositories.
+          </p>
+          {!token && (
             <p>
-              This app aims to address inconvenience of default topics editing
-              interface. Topics Manager will give a dashboard to overview and
-              edit topics of your GitHub repositories.
+              <Link href="/login">
+                <a>Login with GitHub</a>
+              </Link>{" "}
+              to start.
             </p>
-            {!token && (
-              <p>
-                <Link href="/login">
-                  <a>Login with GitHub</a>
-                </Link>{" "}
-                to start.
-              </p>
-            )}
-            {token && (
-              <p>
-                <Link href="/dashboard">
-                  <a>Open dashboard</a>
-                </Link>{" "}
-                to start or{" "}
-                <Link href="/logout">
-                  <a>logout</a>
-                </Link>{" "}
-                to finish the session.
-              </p>
-            )}
-          </Wrapper>
-        </Layout>
-      </>
+          )}
+          {token && (
+            <p>
+              <Link href="/dashboard">
+                <a>Open dashboard</a>
+              </Link>{" "}
+              to start or{" "}
+              <Link href="/logout">
+                <a>logout</a>
+              </Link>{" "}
+              to finish the session.
+            </p>
+          )}
+        </Wrapper>
+      </Layout>
     );
   }
 }
 
 PageIndex.propTypes = {
-  // eslint-disable-next-line react/require-default-props
   token: PropTypes.string
 };
 
