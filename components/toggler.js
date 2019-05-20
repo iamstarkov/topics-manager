8d09febd11c432b0432c24b7217f98e103d6e5cb
Vladimir Starkov <iamstarkov@gmail.com>
2019-05-20 16:31:02 +0200

    feat(components/Toggler): use hooks

--- components/toggler.js
+++ components/toggler.js
@@ -1,30 +1,17 @@
 import React from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { useSelector, useDispatch } from "react-redux";
 import * as ducks from "../ducks";
 
-const Toggler = ({ toggle, isDark }) => (
-  <button type="button" onClick={toggle}>
-    Toggle to {isDark ? "light" : "dark"} theme
-  </button>
-);
-
-Toggler.propTypes = {
-  toggle: PropTypes.func.isRequired,
-  isDark: ducks.theme.shape.isDark
+const Toggler = () => {
+  const dispatch = useDispatch();
+  const isDark = useSelector(ducks.theme.selectors.isDark);
+  const toggle = bindActionCreators(ducks.theme.actions.toggle, dispatch);
+  return (
+    <button type="button" onClick={toggle}>
+      Toggle to {isDark ? "light" : "dark"} theme
+    </button>
+  );
 };
 
-const mapStateToProps = state => ({
-  isDark: ducks.theme.selectors.isDark(state)
-});
-
-const mapDispatchToProps = {
-  toggle: ducks.theme.actions.toggle
-};
-
-const enhance = connect(
-  mapStateToProps,
-  mapDispatchToProps
-);
-
-export default enhance(Toggler);
+export default Toggler;
