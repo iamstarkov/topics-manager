cc5a2041f740d86e615a4fb3086b6c65f7ab6d79
Vladimir Starkov <iamstarkov@gmail.com>
2019-05-20 18:30:28 +0200

    hookify CustomThemeProvider

--- components/custom-theme-provider.js
+++ components/custom-theme-provider.js
@@ -1,24 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { connect } from "react-redux";
+import { useSelector } from "react-redux";
 import { ThemeProvider } from "styled-components";
 import * as ducks from "../ducks";
 
-const CustomThemeProvider = ({ isDark, dark, light, children }) => (
-  <ThemeProvider theme={isDark ? dark : light}>{children}</ThemeProvider>
-);
+const CustomThemeProvider = ({ dark, light, children }) => {
+  const isDark = useSelector(ducks.theme.selectors.isDark);
+  return (
+    <ThemeProvider theme={isDark ? dark : light}>{children}</ThemeProvider>
+  );
+};
 
 CustomThemeProvider.propTypes = {
-  isDark: ducks.theme.shape.isDark,
   dark: PropTypes.shape().isRequired,
   light: PropTypes.shape().isRequired,
   children: PropTypes.node.isRequired
 };
 
-const mapStateToProps = state => ({
-  isDark: ducks.theme.selectors.isDark(state)
-});
-
-const enhance = connect(mapStateToProps);
-
-export default enhance(CustomThemeProvider);
+export default CustomThemeProvider;
