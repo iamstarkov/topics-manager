ade8ccd685821f2ddcaaf3dfb12faa129c879b12
Vladimir Starkov <iamstarkov@gmail.com>
2019-06-14 03:10:11 +0200

    fix oauth for Pull-Request deployments
    fixes #35

--- pages/auth/github/callback.js
+++ pages/auth/github/callback.js
@@ -1,13 +1,39 @@
 import React from "react";
-import Router from "next/router";
+import url from "url";
 import api from "../../../util/api";
 import "../../../util/dotenv-workaround";
 
 const isDev = process.env.NODE_ENV !== "production";
 
+const getDeploymentHost = id => `topics-manager-${id}.now.sh`;
+const omit = (key, object) => {
+  const copyObject = { ...object };
+  delete copyObject[key];
+  return copyObject;
+};
+
 class GithubAuthCallback extends React.Component {
   static async getInitialProps({ res, query }) {
     if (res) {
+      const state = JSON.parse(query.state || {});
+      const { deploymentId } = state;
+
+      if (!isDev && deploymentId) {
+        res.writeHead(302, {
+          Location: url.format({
+            protocol: "https",
+            host: getDeploymentHost(deploymentId),
+            pathname: "/auth/github/callback",
+            query: {
+              ...omit("state", query),
+              state: JSON.stringify(omit("deploymentId", state))
+            }
+          })
+        });
+        res.end();
+        return;
+      }
+
       const response = await api.post(
         `https://github.com/login/oauth/access_token`,
         null,
@@ -31,9 +57,9 @@ class GithubAuthCallback extends React.Component {
       // this page isnt meant to be accessible
       // via client side navigation,
       // thus the best shot is to restart the oauth2 flow
-      Router.replace("/auth/github/");
+      // eslint-disable-next-line no-restricted-globals
+      location.href = "/auth/github";
     }
-    return {};
   }
 }
 
--- pages/auth/github/index.js
+++ pages/auth/github/index.js
@@ -1,28 +1,44 @@
 import React from "react";
-import Router from "next/router";
 import "../../../util/dotenv-workaround";
+import url from "url";
 
 const isDev = process.env.NODE_ENV !== "production";
 
-const baseUrl = isDev
-  ? "http://localhost:3000"
-  : "https://topics-manager.now.sh";
-
-const getRedirectUrl = ({ clientId, callbackUrl, scope }) =>
-  `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${callbackUrl}&scope=${scope}`;
+const deploymentRegExp = /topics-manager-([\w]{9})\.now\.sh/;
+const isStaging = host => deploymentRegExp.test(host);
+const getDeploymentId = host => deploymentRegExp.exec(host)[1];
 
 class GithubAuthRedirect extends React.Component {
-  static async getInitialProps({ res }) {
-    const redirectUrl = getRedirectUrl({
-      clientId: process.env.CLIENT_ID,
-      callbackUrl: `${baseUrl}/auth/github/callback`,
-      scope: "user,public_repo"
-    });
+  static async getInitialProps({ req, res }) {
     if (res) {
+      const {
+        headers: { host }
+      } = req;
+      const redirectUrl = url.format({
+        protocol: "https",
+        host: "github.com",
+        pathname: "/login/oauth/authorize",
+        query: {
+          client_id: process.env.CLIENT_ID,
+          redirect_uri: url.format({
+            protocol: isDev ? "http" : "https",
+            host: isDev ? "localhost:3000" : "topics-manager.now.sh",
+            pathname: "/auth/github/callback"
+          }),
+          scope: "user public_repo",
+          state: JSON.stringify({
+            ...(isStaging(host) ? { deploymentId: getDeploymentId(host) } : {})
+          })
+        }
+      });
       res.writeHead(302, { Location: redirectUrl });
       res.end();
     } else {
-      Router.replace(redirectUrl);
+      // this page isnt meant to be accessible
+      // via client side navigation,
+      // thus the best shot is to restart the oauth2 flow
+      // eslint-disable-next-line no-restricted-globals
+      location.href = "/auth/github";
     }
     return {};
   }
