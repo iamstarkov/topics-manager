72678e7c20880c78cabb72a153555d27738d4306
Vladimir Starkov <iamstarkov@gmail.com>
2019-06-26 20:10:57 +0200

    prevent csrf. fix #56

--- routes/auth/github/index.js
+++ routes/auth/github/index.js
@@ -4,20 +4,51 @@ if (process.env.NOW_REGION === "dev1") {
 }
 
 const url = require("url");
+const crypto = require("crypto");
+const nanoid = require("nanoid");
+
+const isDev = process.env.NOW_REGION === "dev1";
+
+const strToB64 = str => Buffer.from(str).toString("base64");
+
+const checksumFactory = secret => text =>
+  crypto
+    .createHmac("sha256", secret)
+    .update(text)
+    .digest("hex");
+
+const fingerprintify = req => {
+  const ip = req.headers["x-forwarded-for"];
+  const useragent = req.headers["user-agent"];
+  const language = req.headers["accept-language"];
+  const { accept } = req.headers;
+
+  return [ip, useragent, language, accept].join(",");
+};
+
+const checksum = checksumFactory(process.env.CLIENT_SECRET);
 
 /* eslint-disable camelcase */
 module.exports = (req, res) => {
   const deploymentHost = req.headers["x-forwarded-host"];
 
+  const oauthSession = nanoid();
+
   const redirect_uri = "https://topics-manager.now.sh/auth/github/callback";
   const client_id = process.env.CLIENT_ID;
   const scope = "user public_repo";
-  const state = JSON.stringify({
-    deploymentHost,
-    redirectPath: req.query.redirectPath
-  });
+  const state = strToB64(
+    JSON.stringify({
+      deploymentHost,
+      redirectPath: req.query.redirectPath,
+      csrf: checksum(fingerprintify(req) + oauthSession)
+    })
+  );
 
+  const secureFlag = isDev ? "" : "Secure";
   res.writeHead(302, {
+    // limit to 10 minutes to automatically reject stale oauth flows/sessions
+    "Set-Cookie": `oauthSession=${oauthSession}; Max-Age=600; SameSite=Lax; Path=/; HttpOnly; ${secureFlag}`,
     Location: `https://github.com/login/oauth/authorize${url.format({
       query: { client_id, redirect_uri, scope, state }
     })}`
